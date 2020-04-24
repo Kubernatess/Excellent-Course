1e52616bec1b84afa91737f4b047e8832d9fa268
MyCloudStack <865096492@qq.com>
2020-04-24 21:43:08 +0800

    dd

--- WebContent/js/learn.js
+++
@@ -1,6 +0,0 @@
-window.onload=function(){		
-	// 刚加载页面,聚焦第一个子栏目
-	var firstHyperlink=document.querySelector("body>aside>ul>li:first-child>a");
-	var iframe=document.querySelector("body>div>iframe");
-	iframe.src=""+firstHyperlink.href;
-};
