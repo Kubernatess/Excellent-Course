dfc03aad81f5da35523686565fe3c42b8cbc73d4
MyCloudStack <865096492@qq.com>
2020-05-22 14:12:20 +0800

    dd

--- WebContent/js/schedule.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    let hyperlinks = document.getElementsByTagName('a');
-    for(let i=0; i<hyperlinks.length; i++){
-    	let progress = hyperlinks[i].getElementsByTagName("progress")[0];
-        let samp = hyperlinks[i].getElementsByTagName("samp")[0];
-        samp.innerHTML = progress.value+'%';
-    }
-};
